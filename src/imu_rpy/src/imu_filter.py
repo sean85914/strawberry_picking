52cecbc3fb906ed37d0308e0104eccd36a8d356f
sean <sean19960914@gmail.com>
2018-02-27 08:40:58 +0100

    revise imu_filter

--- src/imu_rpy/src/imu_filter.py
+++ src/imu_rpy/src/imu_filter.py
@@ -7,14 +7,15 @@ import tf
 from Adafruit_LSM303 import Adafruit_LSM303
 from Gyro_L3GD20 import Gyro_L3GD20
 
-roll_past = 0
-pitch_past = 0
-yaw_past = 0
 class AdafruitIMU(object):
 	def __init__(self):
 		self.node_name = rospy.get_name()
 		self.compass_accel = Adafruit_LSM303()
 		self.gyro = Gyro_L3GD20()
+		self.roll_past = 0
+		self.pitch_past = 0
+		self.yaw_past = 0
+		self.compl_coeff = 0.97
 		self.pub_timestep = self.setupParam("~pub_timestep", 0.02)
 		self.br = tf.TransformBroadcaster()
 		self.pub_timer = rospy.Timer(rospy.Duration.from_sec(self.pub_timestep), self.publish)
@@ -35,16 +36,16 @@ class AdafruitIMU(object):
 		roll = math.atan2(acc_y, acc_z)
 		pitch = math.atan2(-acc_x, math.sqrt(acc_y * acc_y + acc_z * acc_z))
 		# Complementary filter
-		roll = 0.98 * roll + 0.02 * roll_past
-		pitch = 0.98 * pitch + 0.02 * pitch_past
+		roll = self.compl_coeff * roll + (1 - self.compl_coeff) * self.roll_past
+		pitch = self.compl_coeff * pitch + (1 - self.compl_coeff) * self.pitch_past
 		mag_x_tilt = compass[0] * math.cos(pitch) + compass[1] * math.sin(pitch)
 		mag_y_tilt = compass[0] * math.sin(roll) * math.sin(pitch) + compass[2] * math.cos(roll) - compass[1] * math.sin(roll) * math.cos(pitch)
 		yaw = math.atan2(mag_y_tilt, mag_x_tilt)
-		yaw = 0.98 * yaw + 0.02 * yaw_past
+		yaw = self.compl_coeff * yaw + (1- self.compl_coeff) * self.yaw_past
 		# Update
-		roll_past = roll
-		pitch_past = pitch
-		yaw_past = yaw
+		self.roll_past = roll
+		self.pitch_past = pitch
+		self.yaw_past = yaw
 		# compass[0] > mag_x
 		# compass[1] > mag_z
 		# compass[2] > mag_y
