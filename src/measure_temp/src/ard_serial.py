f131493b5b825e862fc7b5ee80204af8a277d010
seanNCTU <sean19960914.gmail.com>
2018-03-30 23:56:29 +0800

    revise readme, add port param, add launch file

--- src/measure_temp/src/ard_serial.py
+++ src/measure_temp/src/ard_serial.py
@@ -3,13 +3,13 @@
 # and publish as ROS topic 
 # Editor: Sean, Lu
 # Last update: 3/30, 2018
-
+'''
+    Update: add port as parameter (3/30, 2018)
+'''
 import rospy
 import serial
 from std_msgs.msg import Float64
 
-# Remember to permit for arduino first
-ard = serial.Serial('/dev/ttyUSB0', 9600)
 temp_pub = rospy.Publisher('/temp/air_temp', Float64, queue_size = 10)
 
 def main():
@@ -22,5 +22,8 @@ def main():
 
 if __name__ == '__main__':
 	rospy.init_node('ard_serial', anonymous = False)
+	port = rospy.get_param("~port", "/dev/ttyUSB0")
+	# Remember to permit port first
+	ard = serial.Serial(port, 9600)
 	main()
 	rospy.spin()
